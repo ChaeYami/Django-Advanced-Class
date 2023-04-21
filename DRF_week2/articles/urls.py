fc3ee49da67b895a39403acd04fa654684b14ae9
Chaeyeon Seo <ssallyseo@gmail.com>
2023-04-21 18:12:37 +0900

    make CBV

--- DRF_week2/articles/urls.py
+++ DRF_week2/articles/urls.py
@@ -5,6 +5,6 @@ from articles import views
 urlpatterns = [
     
     path("", views.articleAPI, name = "index"), # articles/index/
-    path("<int:article_id>/", views.articleDetailAPI, name = "article_view"), # articles/id/ 
+    path("<int:article_id>/", views.ArticleList.as_view(), name = "article_view"), # articles/id/ 
     
 ]
--- DRF_week2/articles/views.py
+++ DRF_week2/articles/views.py
@@ -5,9 +5,60 @@ from articles.models import Article
 from articles.serializers import ArticleSerializer
 from rest_framework import status # 상태 보내줄 때 사용
 from rest_framework.generics import get_object_or_404 # 없는 글 조회 에러처리에 사용
+from rest_framework.views import APIView # 클래스형 뷰 만들 때 사용
 
 
 # Create your views here.
+
+
+# ------------------------------- 클래스형 뷰 ------------------------------- 
+
+
+# =============== 조회, 생성 (GET, POST) =============== 
+class ArticleList(APIView):
+    
+    # READ
+    def get(self, request, format=None): # => if request.method == 'GET':
+        articles = Article.objects.all() # 데이터 가져오기
+        serializer = ArticleSerializer(articles, many = True) # 데이터 보여주기
+        return Response(serializer.data)
+    # CREATE
+    def post(self, request, format=None): # => request.method == 'POST':
+        serializer = ArticleSerializer(data = request.data) # 데이터 받아오기
+        if serializer.is_valid(): # 유효성 검사 통과
+            serializer.save() # 받아온 데이터 db에 저장
+            return Response(serializer.data, status=status.HTTP_201_CREATED) # 정상 생성 (등록) 상태
+        else: # 유효성 검사 만족 X -> 입력값에 문제
+            print(serializer.errors) 
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) # 에러를 반환, 잘못된 요청 상태
+
+
+
+class ArticleDetail(APIView):
+    # 상세페이지
+    def get(self, request, article_id, format=None): # request.method == 'GET':
+        article = get_object_or_404(Article, id = article_id) # 데이터 가져오기
+        serializer = ArticleSerializer(article) # 데이터 보여주기
+        return Response(serializer.data)
+    # 수정
+    def put(self, request, article_id, format=None): # request.method == 'PUT'
+        article = get_object_or_404(Article, id = article_id) # 데이터 가져오기
+        serializer = ArticleSerializer(article, data = request.data) # 데이터 받아오기 (post 와 get 이 섞인 느낌!)
+        if serializer.is_valid(): # 유효성 검사 통과
+            serializer.save() # 받아온 데이터 db에 저장
+            return Response(serializer.data) # 정상 생성 (등록) 상태
+        else: # 유효성 검사 만족 X -> 입력값에 문제
+            print(serializer.errors) 
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) # 에러를 반환, 잘못된 요청 상태
+    # 삭제
+    def delete(self, request, article_id, format=None): # request.method == 'DELETE':
+        article = get_object_or_404(Article, id = article_id)
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# ------------------------------- 함수형 뷰 ------------------------------- 
+
 
 # =============== 조회, 생성 (GET, POST) =============== 
 
@@ -18,7 +69,6 @@ from rest_framework.generics import get_object_or_404 # 없는 글 조회 에러
 #     articles = articles[0]
 #     serializer = ArticleSerializer(articles)
 #     return Response(serializer.data)
-
 
 #전체 데이터
 @api_view(['GET','POST'])
